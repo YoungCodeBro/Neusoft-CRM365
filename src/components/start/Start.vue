da7c32090982a450a9f15521f3bcfabaddd73d55
laggy <chent3378@163.com>
2022-06-13 19:27:20 +0800

    test2

---
+++ src/components/start/Start.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="title">
+      <img src="../../assets/restaurant.svg" alt="用餐图标" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css" scoped>
+</style>
