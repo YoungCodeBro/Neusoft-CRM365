f981137a920769930f3670e6460360e4d4f7d908
YoungCodeBro <1141613534@qq.com>
2022-06-14 17:27:41 +0800

    初步完成了点餐页面的框架，待完善点餐逻辑与模块间接口对接。

--- src/components/liutao/Side.vue
+++ src/components/liutao/Side.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    <van-icon name="bars" size="50px" @click="showPopup" />
-    <van-popup v-model="show" position="left" :overlay="true" closeable  close-icon="close" close-icon-position="bottom-right"  >
+    <van-sticky offset-top="20rem">
+      <button class="nav" @click="showPopup" :style="{left:leftValue}">
+        <img :src="require('../../assets/Image/nav.png')"  alt="">
+        <span id="navBut">菜单</span>
+      </button>
+    </van-sticky>
+
+    <van-popup v-model="show" position="left" :overlay="false">
       <van-sidebar v-model="activeKey" >
-        <van-sidebar-item title="标签名称" />
-        <van-sidebar-item title="标签名称" />
-        <van-sidebar-item title="标签名称" />
+        <van-sidebar-item title="精选热菜" @click="scrollToView('精选热菜')" />
+        <van-sidebar-item title="特色菜" @click="scrollToView('特色菜')"/>
+        <van-sidebar-item title="城新小炒"  @click="scrollToView('城新小炒')"/>
+        <van-sidebar-item title="田园时蔬"  @click="scrollToView('田园时蔬')"/>
+        <van-sidebar-item title="粗粮主食"  @click="scrollToView('粗粮主食')"/>
       </van-sidebar>
     </van-popup>
   </div>
@@ -18,13 +26,37 @@ export default {
     return {
       show: false,
       activeKey: 0,
-    }
+      leftValue: '-1rem',
+  }
   },
   methods: {
     showPopup(){
-      this.show = !this.show;
+      if(document.getElementById('navBut').innerHTML==='返回'){
+        this.$router.push('/menu');
+        document.getElementById('navBut').innerHTML = '菜单';
+      }else{
+        this.show = !this.show;
+        if(this.leftValue==='-1rem'){
+          this.leftValue='4rem';
+        }else{
+          this.leftValue='-1rem';
+        }
+      }
     },
+    scrollToView(title){
+      this.showPopup();
+    //先获取目标位置距离
+      let targetbox= document.getElementById(title);
+      this.target= targetbox.offsetTop-80;
+      console.log(this.target);
 
+    //再滑动指定距离
+      window.scrollTo({
+        top:this.target,
+        left:0,
+        behavior:"smooth"
+      });
+    }
   }
 }
 </script>
@@ -45,4 +77,27 @@ export default {
   border-radius: 30px;
   border:10px solid red;
 }
+.nav{
+  left: -1rem;
+  position: absolute;
+  top: 42%;
+  background-color: rgb(132,128,128,0.5);
+  width: 5rem;
+  height: 5rem;
+  text-align: center;
+  border-bottom-right-radius: 60%;
+  border-top-right-radius: 60%;
+  z-index: 2;
+  transition: 0.25s linear;
+}
+.nav img{
+  height: 2.5rem;
+  width: 3rem;
+  margin-top: 0.5rem;
+  margin-left: 0.5rem;
+}
+.nav span{
+  color: #fff;
+}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,10 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Start from '../components/start/Start.vue'
 import Menu from '../components/liutao/Menu'
-
+import OrderFood from '../components/liutao/OrderFood'
+import HotFood from '../components/liutao/HotFood'
+import Ordered from '../components/liutao/Ordered'
+import Like from '../components/liutao/Like'
 Vue.use(Router)
 
 export default new Router({
@@ -21,7 +24,29 @@ export default new Router({
     {
       path: '/menu',
       name: 'Menu',
-      component: Menu
+      component: Menu,
+      children: [
+        {
+          path: '/',
+          name: 'orderFood',
+          component: OrderFood,
+        },
+        {
+          path: '/menu/hot',
+          name: 'hotFood',
+          component: HotFood,
+        },
+        {
+          path: '/menu/ordered',
+          name: 'ordered',
+          component: Ordered,
+        },
+        {
+          path: '/menu/like',
+          name: 'like',
+          component: Like,
+        },
+      ]
     }
   ]
 })
