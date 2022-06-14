a17c59e9fd65b14cb70c7456d7eb64de66445c80
laggy <chent3378@163.com>
2022-06-14 09:15:29 +0800

    修改素材图片

---
+++ src/components/nav/NavButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <van-button round>按钮</van-button>
+        <van-overlay>
+
+        </van-overlay>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="css" scoped>
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Start from '../components/start/Start.vue'
+import NavButton from '../components/nav/NavButton.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/start',
       name: 'Start',
       component: Start
+    },
+    {
+      path: '/nav',
+      name: 'Nav',
+      component: NavButton
     }
   ]
 })
